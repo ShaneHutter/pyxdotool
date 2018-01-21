9020066a4cd6aa716338efe4cc9e9b451013a47f
Shane Hutter <shane.hutter86@gmail.com>
2018-01-21 20:42:12 +1100

    xdotool.window

---
+++ pyxdotool/window.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+    pyxdotool
+        pyxdotool is a python wrapper for using xdotool as a
+        python module.
+
+        Written by: Shane Hutter
+        
+        Required Dependancies: python >= 3.5, xdotool
+
+        This python script, and all of pyxdotool is licensed
+        under the GNU GPL version 3
+
+        pyxdotool is free software; you can redistribute it and/or modify
+        it under the terms of the GNU Lesser General Public License as published
+        by the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        pyxdotool is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+        GNU Lesser General Public License for more details.
+        You should have received a copy of the GNU Lesser General Public License
+        along with this program. If not, see <http://www.gnu.org/licenses/>.  
+"""
+
+from . import shell
+
